a47a8979546515dcf89d9efe4c0b5223678a914e
tkacza <kaczatom4@gmail.com>
2019-11-11 19:50:58 +0100

    end of lesson

--- Common/src/main/java/pl/c0/kaczuch/calculator/Main.java
+++ Common/src/main/java/pl/c0/kaczuch/calculator/Main.java
@@ -1,14 +1,32 @@
 package pl.c0.kaczuch.calculator;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Calendar;
 import java.util.Scanner;
 
 public class Main
 {
-    public static void main(String[] args)
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException
     {
         System.out.println("Main");
-        IIICalculator cal = new CCCalculator();
-        cal.run(cal);
+
+        String current = new java.io.File(".").getCanonicalPath();
+        System.out.println(current);
+        File f = new File("./Common/src/main/java/pl/c0/kaczuch/calculator/plugins/Plugin.jar");
+//        System.out.println(f.exists());
+
+        ClassLoader c = ClassLoader.getSystemClassLoader();
+
+
+        System.out.println( c.loadClass(f.getName()));
+
+
+
+
+
+//        IIICalculator cal = new CCCalculator();
+//        cal.run(cal);
     }
 }
