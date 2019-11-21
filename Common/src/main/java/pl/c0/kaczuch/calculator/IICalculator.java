a39927e180d93d54e18557d96a614caee6a020d0
Tomasz <kaczatom4@gmail.com>
2019-11-21 23:47:04 +0100

    I don't know why but when I've changed name ICalculator on IICalculator it works

--- Common/src/main/java/pl/c0/kaczuch/calculator/IICalculator.java
+++ Common/src/main/java/pl/c0/kaczuch/calculator/IICalculator.java
@@ -1,11 +1,10 @@
 package pl.c0.kaczuch.calculator;
 
-public interface ICalculator
+public interface IICalculator
 {
     public double add(double a, double b);
     public double subtraction(double a, double b);
     public double multiplication(double a, double b);
     public double division(double a, double b);
-    public void run(ICalculator cal);
+    public void run(IICalculator cal);
 }
-
